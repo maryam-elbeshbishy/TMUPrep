4a27d3af1b7d2777a92c00f6db5491aed42cf8d1
igorpenedos <59774723+igorpenedos@users.noreply.github.com>
2023-10-06 16:06:29 -0400

    mongodb basic connection setup

---
+++ server/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+    "context"
+    "log"
+
+    "github.com/gin-gonic/gin"
+
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const uri = "connection string"
+
+var mongoClient *mongo.Client
+
+func init() {
+    if err := connect_to_mongodb(); err != nil {
+        log.Fatal("Could not connect to MongoDB")
+    }
+}
+
+
+func main() {
+	r := gin.Default()
+
+    r.GET("/", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "message": "Hello World",
+        })
+    })
+
+    r.Run()
+}
+
+func connect_to_mongodb() error {
+    serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	
+    opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+
+    client, err := mongo.Connect(context.TODO(), opts)
+
+    if err != nil {
+        panic(err)
+    }
+    err = client.Ping(context.TODO(), nil)
+    mongoClient = client
+    return err
+}
