ee6c63ecf71277efb07b90d11cc45ce90e549175
Maher Muhtadi <m.muhtadi2002@gmail.com>
2023-10-12 21:35:38 -0400

    Error resolved: connects to the correct db now

--- server/scraper/addCourses.py
+++ server/scraper/addCourses.py
@@ -2,11 +2,9 @@ from pymongo import MongoClient, InsertOne
 import json
 
 CONNECTION_STRING = "mongodb+srv://drice:0i2psWT09d3ZtNMq@projects.hx2wxbo.mongodb.net/tmuprep?authSource=admin&replicaSet=atlas-j2yill-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
-DATABASE = "tmuprep"
-collection = "course"
 
 client = MongoClient(CONNECTION_STRING)
-db = client.DATABASE
+db = client.tmuprep
 collection = db.course
 requesting = []
 
