84c07def4f5d2d4591a2051c0cbca94e86d6d429
igorpenedos <59774723+igorpenedos@users.noreply.github.com>
2023-11-10 18:17:26 -0500

    add pagination for GET all courses

--- server/routes/courseRoutes/course.go
+++ server/routes/courseRoutes/course.go
@@ -1,19 +1,57 @@
 package courseRoutes
 
 import (
+	"math"
 	"net/http"
+	"strconv"
 	"tmuprep/models"
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func Routes(router *gin.RouterGroup, mongoDB *mongo.Client) {
-	router.GET("/", func(c *gin.Context) {
+	/*
+		{
+			page: int,
+			limit: int
+		}
+	*/
+	router.GET("", func(c *gin.Context) {
+		l, _ := c.GetQuery("limit")
+		p, _ := c.GetQuery("page")
+
+		limit, _ := strconv.Atoi(l)
+		page, _ := strconv.Atoi(p)
+
+		count, err := mongoDB.Database("tmuprep").Collection("courses").EstimatedDocumentCount(c)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			c.Abort()
+			return
+		}
+		var numberOfPages float64
+		if limit > 0 {
+			numberOfPages = math.Ceil(float64(count) / float64(limit))
+
+			if float64(page) > float64(numberOfPages) {
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"error": "Pagination params outside of scope",
+				})
+				c.Abort()
+				return
+			}
+		}
+
+		paginationOptions := options.Find().SetLimit(int64(limit)).SetSkip(int64(page))
+
 		var courses []models.Course
 
-		cursor, err := mongoDB.Database("tmuprep").Collection("courses").Find(c, bson.D{{}})
+		cursor, err := mongoDB.Database("tmuprep").Collection("courses").Find(c, bson.D{{}}, paginationOptions)
 
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{
@@ -30,7 +68,8 @@ func Routes(router *gin.RouterGroup, mongoDB *mongo.Client) {
 		}
 
 		c.JSON(http.StatusOK, gin.H{
-			"courses": courses,
+			"courses":       courses,
+			"numberOfPages": numberOfPages,
 		})
 	})
 	router.GET("/:courseID", func(c *gin.Context) {
