bb506fff9c0b254b9be6edf55dcf9300cb2be30a
igorpenedos <59774723+igorpenedos@users.noreply.github.com>
2023-11-10 17:07:42 -0500

    add GET call for all courses

--- server/routes/courseRoutes/course.go
+++ server/routes/courseRoutes/course.go
@@ -1,43 +1,43 @@
 package courseRoutes
 
 import (
-	models "tmuprep/models"
+	"net/http"
+	"tmuprep/models"
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func Routes(router *gin.RouterGroup, mongoDB *mongo.Client )  {
-	router.GET("", func (c *gin.Context)  {
-		// courseID := c.Param("courseID")
-		// cursor, err := mongoDB.Database("tmuprep").Collection("course").Find(c, bson.D{{"courseCode", courseID}})
-		// if err != nil {
-		// 	panic(err)
-		// }
-		// var results []models.Course
-		// if err = cursor.All(c, &results); err != nil {
-		// 	panic(err)
-		// }
-		// for _, result := range results {
-		// 	res, _ := json.Marshal(result)
-			
-		// 	fmt.Println(string(res))
-		// }
+func Routes(router *gin.RouterGroup, mongoDB *mongo.Client) {
+	router.GET("/", func(c *gin.Context) {
+		var courses []models.Course
 
-		// c.JSON(200, gin.H{
-		// 	oid: result.
-		// 	"message": "course",
-		// })
-		c.JSON(200, gin.H{
-			"message": "all courses",
+		cursor, err := mongoDB.Database("tmuprep").Collection("courses").Find(c, bson.D{{}})
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			c.Abort()
+			return
+		}
+
+		if err = cursor.All(c, &courses); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"courses": courses,
 		})
 	})
-	router.GET("/:courseID", func (c *gin.Context)  {
+	router.GET("/:courseID", func(c *gin.Context) {
 		courseID := c.Param("courseID")
 
 		var course models.Course
-		err := mongoDB.Database("tmuprep").Collection("course").FindOne(c, bson.D{{"courseCode", courseID}}).Decode(&course)
+		err := mongoDB.Database("tmuprep").Collection("courses").FindOne(c, bson.D{{"courseCode", courseID}}).Decode(&course)
 		if err != nil {
 			if err == mongo.ErrNoDocuments {
 				c.JSON(400, gin.H{
@@ -49,8 +49,8 @@ func Routes(router *gin.RouterGroup, mongoDB *mongo.Client )  {
 		}
 
 		c.JSON(200, gin.H{
-			"oid": course.ID,
-			"courssName": course.CourseCode,
+			"oid":        course.ID,
+			"courseName": course.CourseCode,
 		})
 	})
 }
