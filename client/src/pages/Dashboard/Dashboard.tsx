acf21e1269c49f7ec4e5ef1d85c510132df3d8dc
Abdullah Aftab <abdullahaftabwe@gmail.com>
2023-11-26 22:41:03 -0500

    Added Search functionality for SideBar.tsx/removed one test line of code

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,16 +1,10 @@
 import { Box, Flex } from '@chakra-ui/react'
 import Schedule from './components/Schedule'
-import DashboardCourse from '../../components/DashboardCourse'
 import Sidebar from './components/SideBar.tsx'
 
 const Dashboard = () => {
     return (
         <Box bg="surface.dark" w="100vw" h="100vh" pt="80px">
-            <DashboardCourse
-                courseCode="CPS109"
-                courseName="Computer Science I"
-                dropCourse={() => {}}
-            />
             <Flex flexDir="row" h="100%" gap="40px">
                 <Sidebar />
                 <Schedule />
--- client/src/pages/Dashboard/components/SideBar.tsx
+++ client/src/pages/Dashboard/components/SideBar.tsx
@@ -16,10 +16,15 @@ interface CourseType {
 const Sidebar = () => {
     const cookies = new Cookies()
     const [courses, setCourses] = useState<CourseType[]>([])
-    const [search] = useState<string>('')
     const [page, setPage] = useState<number>(1)
     const [totalPages, setTotalPages] = useState<number>(0)
     const [limit] = useState<number>(10)
+
+    const [search, setSearch] = useState<string>('')
+    const updateCourseList = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearch(event.target.value)
+    }
+
     useEffect(() => {
         // Fetch courses from the endpoint
         axios
@@ -34,30 +39,35 @@ const Sidebar = () => {
             .then(response => {
                 setCourses(response.data.courses)
                 setTotalPages(response.data.numberOfPages)
+                console.log(response.data.courses)
             })
             .catch(error => {
                 console.error('Error fetching courses:', error)
             })
     }, [page, search])
     const displayCourses = () => {
-        return courses.map(course => {
-            return (
-                <CourseSearchOption
-                    key={course.courseCode}
-                    code={course.courseCode}
-                    name={course.title}
-                    description={course.description}
-                    prereqs={course.prerequisites}
-                    antireqs={course.antirequisites}
-                />
-            )
-        })
+        if (courses != null) {
+            return courses.map(course => {
+                return (
+                    <CourseSearchOption
+                        key={course.courseCode}
+                        code={course.courseCode}
+                        name={course.title}
+                        description={course.description}
+                        prereqs={course.prerequisites}
+                        antireqs={course.antirequisites}
+                    />
+                )
+            })
+        }
     }
 
     return (
         <Box bg="secondary.main" w="30%" h="100%" overflowY="auto" p="20px">
             <VStack spacing="20px">
                 <Input
+                    value={search}
+                    onChange={updateCourseList}
                     bg="surface.main"
                     border="none"
                     borderRadius="10px"
